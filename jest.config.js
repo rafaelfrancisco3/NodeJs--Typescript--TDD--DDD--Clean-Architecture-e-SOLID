23b27331b2aff9c4d7cd5fdb64acee1c5d189501
Rafael <franciscorafael814@gmail.com>
2022-01-08 16:08:39 -0400

    fix: jest was fixed

--- jest.config.js
+++ jest.config.js
@@ -18,12 +18,17 @@ module.exports = {
 
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  roots: ['<rootDir>/src'],
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectoryFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
